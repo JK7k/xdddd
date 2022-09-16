0855199fd2b744bcaaec71742a40ab166d520032
JK7k <97955908+JK7k@users.noreply.github.com>
2022-09-16 11:32:55 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,23 @@
+var iloscZn=8;
+var iloscWolnychZnakow=0;
+var iloscDuzeL=0;
+var iloscMaleL=0;
+function ustawWolne(){
+    iloscWolnychZnakow=+iloscZn-(+iloscDuzeL+ +iloscMaleL);
+    document.getElementById("iloscDuzeL").max=+iloscWolnychZnakow+ +iloscDuzeL;
+    document.getElementById("iloscMaleL").max=+iloscWolnychZnakow+ +iloscMaleL;
+}
+function wybieranieDuzeL(){
+    iloscDuzeL=document.getElementById("iloscDuzeL").value;
+    document.getElementById("wybDuzeL").value=iloscDuzeL;
+    ustawWolne()
+}
+function wybieranieMaleL(){
+    iloscMaleL=document.getElementById("iloscMaleL").value;
+    document.getElementById("wybMaleL").value=iloscMaleL;
+    ustawWolne()
+}
+function iloscZnakow(){
+    iloscZn=document.getElementById("iloscZnakow").value;
+    ustawWolne()
+}
